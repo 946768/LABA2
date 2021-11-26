eecdc9d7d957e285d03507145bcfb50b2d94e794
medvedevaJ <jule4ka03@mail.ru>
2021-11-26 15:59:24 +0300

    /

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,14 +6,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-           
-                        string s = Console.ReadLine();
-                        for (int i = s.Length - 1; i >= 0; i--)
-                        {
-                            Console.Write(s[i]);
-                        }
-                        Console.ReadLine();
-                
+            string s = Console.ReadLine();
+
+            for (int i = s.Length - 1; i >= 0; i--)
+
+            {
+                Console.Write(s[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
